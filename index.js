6a1d9bc2e50d8da4b0887ab9325f5997d08b748a
vigneshdevelopr <vigneshtpr99@gmail.com>
2024-09-06 13:46:36 +0530

    fix

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use("/user", signupRouter);
 app.use("/user", loginRouter);
 app.get('/',(req,res)=>{
-  return res.status(200).json({message: 'Stackoverflow server working'})
+  return res.status(200).json({message: 'server working'})
 })
 
 
